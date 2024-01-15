136ae1a380472b0e543e6f2c865647aa4ea02438
ignacioaresm <ignacioaresm@gmail.com>
2024-01-15 17:35:08 -0300

    version 0.5

--- js/main.js
+++ js/main.js
@@ -1,32 +1,75 @@
-console.log('Inicio del Programa')
+const productos = [
+  { nombre: "manga1", precio: 4500 },
+  { nombre: "manga2", precio: 4500 },
+  { nombre: "manga3", precio: 4500 },
+  { nombre: "manga4", precio: 6000 },
+  { nombre: "manga5", precio: 6000 },
+  { nombre: "manga6", precio: 6000 },
+];
 
-// Declarar función
-function preguntar(pregunta) {
-    return prompt(pregunta);
-}
-  
-// Preguntar nombre
-let nombre = preguntar("¿Cuál es tu nombre?");
- 
-// Preguntar edad
-let edad = parseInt(preguntar("¿Cuántos años tenés?"));
-  
-// Preguntar sexo
-let sexo = preguntar("¿Eres hombre o mujer?");
-  
-// Resultados
-let resultado = "";
-  if (edad <= 10) {
-    resultado = "Kodomo";
-  } else if (edad <= 18 && sexo === "hombre") {
-    resultado = "Shōnen";
-  } else if (edad <= 18 && sexo === "mujer") {
-    resultado = "Shōjo";   
-  } else if (sexo === "hombre") {
-    resultado = "Seinen";
-  } else if (sexo === "mujer") {
-    resultado = "Josei";
+let carrito = []
+
+let seleccion = prompt("Bienvenidx! ¿Deseas comprar algún Manga? (si o no)")
+
+while(seleccion !="si" && seleccion !="no"){
+  alert("Por favor ingresá si o no")
+  seleccion = prompt("¿Deseas comprar algún Manga? (si o no)")
 }
 
-// Mostrar mensaje final
-alert(`Bienvenid(x) a la Tribu, ${nombre}! Te recomendamos que busques tus mangas favoritos en la sección ${resultado} de nuestra web, Kanpai ;)`);
+if (seleccion == "si"){
+  alert("Estos son los Mangas que tenemos en stock")
+  let stockProductos = productos.map((productos) => productos.nombre + " " + "$" + productos.precio);
+  alert(stockProductos.join(", ")) 
+} else if (seleccion == "no"){
+  alert("Gracias por visitar nuestra tienda, te esperamos la próxima ;)")
+}
+
+while(seleccion != "no"){
+  let producto = prompt ("Agrega un Manga a tu carrito")
+  let precio = 0
+
+  if(producto == "manga1" || producto == "manga2" || producto == "manga3" || producto == "manga4" || producto == "manga5" || producto == "manga6"){
+    switch(producto){
+      case "manga1":
+        precio = 4500;
+      break;
+      case "manga2":
+        precio = 4500;
+      break;
+      case "manga3":
+        precio = 4500;
+      break;
+      case "manga4":
+        precio = 6000;
+      break;
+      case "manga5":
+        precio = 6000;
+      break;
+      case "manga6":
+        precio = 6000;
+      break;
+      default:
+      break;
+    }
+    let unidades = parseInt(prompt("Por favor indicá la cantidad de unidades"))
+    carrito.push({producto, unidades, precio})
+  } else{
+    alert("No contamos con stock de ese Manga")
+  }
+  seleccion = prompt("¿Querés agregar otro producto? (si o no)")
+  while(seleccion !="si" && seleccion !="no"){
+    alert("Por favor ingresá si o no")
+    seleccion = prompt("¿Querés agregar otro producto? (si o no)")
+  }
+  while(seleccion == "no"){
+    alert("¡Gracias por tu compra!")
+    carrito.forEach((carritoTotal) => {
+      console.log(`Producto: ${carritoTotal.producto}, Unidades: ${carritoTotal.unidades}, Precio por unidad: $${carritoTotal.precio}, Total por producto: $${carritoTotal.unidades * carritoTotal.precio}`)
+    })
+  break;
+  }
+}
+
+const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0)
+console.log(`El total a pagar por la compra es: $${total}`)
+alert(`El total a pagar es: $${total}`)
